2cc2fabb91d92eea954ad6806b315e6cc2d4d2fc
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-08-16 14:44:05 +0200

    Had issues with variable, that did not change, since i did not reference own instance

--- camera/app/adaptors/camera_adaptor.py
+++ camera/app/adaptors/camera_adaptor.py
@@ -1,47 +1,75 @@
 import cv2
+import datetime
+import time
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
+video_capture = cv2.VideoCapture(0)
 
 class CameraAdaptor:
-
     def __init__(self):
-        cap = cv2.VideoCapture(0)
+        self.detection = False
+        self.detection_stopped_time = None
+        self.timer_started = False
+        self.SECONDS_TO_RECORD_AFTER_DETECTION = 5
+        self.out = None
 
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-        body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+        startCapture(self, video_capture)
 
-        recording = True
-        
-        frame_size = (int(cap.get(3)), int(cap.get(4)))
-        fourcc = cv2. VideoWriter_fourcc(*"mp4v")
-        out = cv2.VideoWriter("video.mp4", fourcc, 20, frame_size)
-
-
-        while True:
-            ret, frame = cap.read()
-            if frame is None:
-                break
-
-            detectAndCapture(frame, face_cascade, body_cascade)
-
-            if cv2.waitKey(1) == ord('q'):
-                break
-    
-        cap.release()
+        video_capture.release()
         cv2.destroyAllWindows()
     
               
+def startCapture(self, video_capture):
+    while True:
+        ret, frame = video_capture.read()
+        if frame is None:
+            break
+
+
+        detectAndCapture(self, frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+      
     
-    
-def detectAndCapture(frame,face_cascade, body_cascade):
+def detectAndCapture(self, frame):    
+
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grey, 1.3, 5)
     bodies = body_cascade.detectMultiScale(grey, 1.3, 5)
+    
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+
+    frame_size = (int(video_capture.get(3)), int(video_capture.get(4))) 
 
     if len(faces) + len(bodies) > 0:
-        recording = True
+        if self.detection:
+            self.timer_started = False
+        else:
+            self.detection = True
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            self.out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20, frame_size)
+            print("Started Recording!")
+    if self.detection:
+        if self.timer_started:
+            if time.time() - self.detection_stopped_time >= self.SECONDS_TO_RECORD_AFTER_DETECTION:
+                self.detection = False
+                self.timer_started = False
+                self.out.release()
+                print("stopped Recording!")
+        else:
+            self.timer_started = True
+            self.detection_stopped_time = time.time()
 
-    cv2.imshow("Camera", frame)
+    if self.detection:
+        self.out.write(frame)
 
-CameraAdaptor()
+    
+
+
 
         
 
