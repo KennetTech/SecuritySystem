ce08744b470156e8a7fde8bd9dddd26db133b35d
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-08-19 16:01:55 +0200

    Setting up connection to RabbitMQ

--- camera/app/entrypoints/server.py
+++ camera/app/entrypoints/server.py
@@ -1,10 +1,29 @@
 from app.adapters.camera_adapter import Camera
+from app.adapters.rabbitmq_adapter import RabbitMQAdapter
+from dotenv import load_dotenv
+import pika
+import os
 
 from fastapi import FastAPI
 
-camera = Camera()
-
 app = FastAPI()
+
+# Load environment variables
+load_dotenv()
+
+# Retrieve environment variables
+RABBITMQ_URL = os.environ.get("RABBITMQ_URL")
+
+# connect to rabbitmq
+params = pika.URLParameters(RABBITMQ_URL)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='videofeedservice')
+
+
+# starting camera module to record intruders
+##camera = Camera()
+
 
 @app.get("/")
 async def root():
