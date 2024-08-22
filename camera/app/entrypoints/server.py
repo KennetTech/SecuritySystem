4bbd166d49e615961f419e5e869b3c6c966d8c91
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-08-22 10:16:32 +0200

    initial commit for backend

--- camera/app/entrypoints/server.py
+++ camera/app/entrypoints/server.py
@@ -1,5 +1,5 @@
 from app.adapters.camera_adapter import Camera
-from app.adapters.rabbitmq_adapter import RabbitMQAdapter
+
 from dotenv import load_dotenv
 import pika
 import os
@@ -8,16 +8,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-rabbit = RabbitMQAdapter()
-
-# rabbit.publish_message('Hello from Camera service!')
-
 # starting camera module to record intruders
-##camera = Camera()
+camera = Camera()
 
 
 @app.get("/")
 async def root():
     return {"message": "hello world"}
-
